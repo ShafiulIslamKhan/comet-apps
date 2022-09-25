10a70a2a37360676f3c9fcfbca222e855a193019
admin <@gmail.com>
2022-09-25 16:09:49 +0600

    Admin auth done

---
+++ app/Http/Controllers/Admin/AdminPageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdminPageController extends Controller
+{
+     /**
+     * Show admin dashboard
+     */
+    public function showDashboard()
+    {
+        return view( 'admin.pages.dashboard' );
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminAuthController;
+use App\Http\Controllers\Admin\AdminPageController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get( '/' , function(){
-    return view( 'admin.pages.dashboard' );
-} );
+//Admin auth routes
+Route::get( '/admin-login', [ AdminAuthController::class, 'showLoginPage' ] ) -> name( 'admin.login.page' );
+Route::post( '/admin-login', [ AdminAuthController::class, 'login' ] ) -> name( 'admin.login' );
+
+//Admin page routes
+Route::get( '/dashboard', [ AdminPageController::class, 'showDashboard' ] ) -> name( 'admin.dashboard' );
